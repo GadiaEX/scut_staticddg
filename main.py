62f9278c081bf52c2f24ed581c931873f9ee6c2a
GadiaEX <934540608@qq.com>
2023-04-05 16:34:28 +0800

    add:test file.@TODO:comment error

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import src.CFGBuilder
 import src.DDGBuilder
+import json
 
-def main(file_name:str):
-    with open(file_name, 'r') as src_file:
-        src_code = src_file.read()
+def build_once(src_code: str) -> None:
     cfg = src.CFGBuilder.CFGVisitor(src_code)
     cfg.build_cfg()
     code = cfg.export_cfg_to_mermaid()
@@ -13,5 +12,13 @@ def main(file_name:str):
     code = ddg.export_mermaid_code()
     print(code)
 
+def main(file_name:str):
+    with open(file_name, 'r') as src_file:
+        json_codes: list = src_file.readlines()
+
+    for each_json_raw_code in json_codes:
+        json_code = json.loads(each_json_raw_code)
+        build_once(json_code['code'])
+
 if __name__ == '__main__':
-    main('target.py')
+    main('python_test_0.jsonl')
